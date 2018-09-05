2157c7c8d82d0f46b75ade7dd53b021bcd885e85
milto <mil.tovar@udla.edu.co>
2018-09-05 17:04:14 -0500

    Generar individuo

--- Boxeadores/Boxeadores/Boxeador.cs
+++ Boxeadores/Boxeadores/Boxeador.cs
@@ -28,14 +28,73 @@ namespace Boxeadores
             Random aleatorio = new Random();
             int ale = aleatorio.Next(3);
             individuo.Brazos = conversion(ale);
-            return individuo;
+            individuo.Genero = false;
+            individuo.Estamina = aleatorios();
+            individuo.Rapidez = aleatorios();
+           return individuo;
         }
 
         private bool[] conversion(int dato)
         {
             bool[] dato_conver = new bool[1];
-
+            #region Código de convertir decimales a binarios con ciclos
+            /*int contador = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                dato_conver[i] = false;
+            }
+            while (((dato / 2) >= 1))
+            {
+                int dato_dividido = dato / 2;
+                int modulo = dato % 2;
+                dato = dato_dividido;
+                if (modulo == 0)
+                {
+                    dato_conver[contador] = true;
+                }
+                else
+                {
+                    dato_conver[contador] = false;
+                }
+                contador++;
+            }*/
+            #endregion
+            #region Código conversión números pequeños
+            switch (dato)
+            {
+                case 0:
+                    dato_conver[0] = false;
+                    dato_conver[1] = false;
+                    break;
+                case 1:
+                    dato_conver[0] = true;
+                    dato_conver[1] = false;
+                    break;
+                case 2:
+                    dato_conver[0] = false;
+                    dato_conver[1] = true;
+                    break;
+                case 3:
+                    dato_conver[0] = true;
+                    dato_conver[1] = true;
+                    break;
+            }
+            #endregion
             return dato_conver;
+        }
+        
+        private bool aleatorios()
+        {
+            Random aleatorio = new Random();
+            int ale = aleatorio.Next(1);
+            if (ale == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
